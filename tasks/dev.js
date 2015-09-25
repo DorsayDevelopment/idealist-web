484e90b64b25f030371c0f28fd736289cb2e4430
Brycen <dorsay@live.ca>
2015-09-24 23:13:18 -0700

    Dev gulp task

--- tasks/dev.js
+++ tasks/dev.js
@@ -1,15 +1,40 @@
 var gulp = require('gulp');
+var exec = require('child_process').exec;
 var plugins = require('gulp-load-plugins')();
 
+
+gulp.task('styles', function() {
+  return gulp.src('./scss/**/*.scss')
+    .pipe(plugins.sass().on('error', plugins.sass.logError))
+    .pipe(gulp.dest('./scss/'))
+    .pipe(plugins.livereload());
+});
+
 gulp.task('watch', function() {
-  // styles
-  // html
-  // js
-  gulp.watch('./scss/**/*.scss', ['styles'])
+  plugins.livereload.listen();
+  gulp.watch('./scss/**/*.scss', ['styles']);
+  gulp.watch('./pages/**/*.js', ['lint']);
+  gulp.watch('./shared/**/*.js', ['lint']);
 });
 
 gulp.task('serve', function() {
-
+  plugins.nodemon({
+    script: 'server.js',
+    ext: 'js html',
+    env: {'NODE_ENV' : 'development'}
+  })
 });
 
-gulp.task('dev', ['serve', 'watch']);
+
+gulp.task('lint', function() {
+  return gulp.src([
+    './pages/**/*.js',
+    './shared/**/*.js'
+  ])
+    .pipe(plugins.jslint({
+      reporter: 'default'
+    }))
+});
+
+
+gulp.task('dev', ['styles', 'serve', 'watch']);
--- tasks/prod.js
+++ tasks/prod.js
@@ -2,18 +2,16 @@ var gulp = require('gulp');
 var plugins = require('gulp-load-plugins')();
 var del = require('del');
 
-gulp.task('styles', function() {
+gulp.task('styles-prod', ['clean'], function() {
   return gulp.src('./scss/**/*.scss')
     .pipe(plugins.sass().on('error', plugins.sass.logError))
     .pipe(plugins.minifyCss())
-    .pipe(gulp.dest('./dist/css/'));
+    .pipe(gulp.dest('./scss/'));
 });
 
-gulp.task('clean', function() {
+gulp.task('clean-prod', function() {
   return del(['dist']);
 });
 
 
-
-
-gulp.task('build-prod', ['clean', 'styles']);
+gulp.task('build-prod', ['clean-prod', 'styles-prod']);
